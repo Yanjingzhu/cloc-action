316fac77e1d6f09d8d70ec52ebd728e2513d4cb3
changenameYanjingzhu <yanjingzhu123@outlook.com>
2020-03-24 18:38:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 cloc $(git rev-parse HEAD) | tee -a cloc.txt
 line=$( tail -n 2 cloc.txt | head -1 )
 num=$( echo ${line} | awk -F" " '{ print $5}')
-echo "::set-output name=lines::$num"
+echo "::set-output name=lines::$line"
